1bac09237a482b516b551e24449b0bf66262c5a5
Imy <imypullan@gmail.com>
2021-06-13 12:37:13 +0100

    final commit

--- src/Components/Parent/TextElement/index.js
+++ src/Components/Parent/TextElement/index.js
@@ -4,7 +4,7 @@ const TextElement = () => {
     return (
         <div>
             <p>
-                This page should give you a count of the number of days from today to Christmas Day, New Years Day and Valentines Day.
+                This page should give you a count of the number of days from today to Christmas Day, New Year's Day and Valentine's Day.
             </p>
         </div>
     )
