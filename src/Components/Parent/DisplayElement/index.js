18e66f8ffe3ff0b63e49b62c3b90820e2410aa4c
Imy <imypullan@gmail.com>
2021-06-13 12:09:18 +0100

    displayElement now a class

--- src/Components/Parent/DisplayElement/index.js
+++ src/Components/Parent/DisplayElement/index.js
@@ -4,12 +4,28 @@ class DisplayElement extends React.Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            dates: this.props.dates
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.dates !== prevProps.dates) {
+            this.setState({
+                dates: this.props.dates
+            })
+        }
     }
 
     render() {
         return (
             <div>
                 <ul>
+                    {this.state.dates.map(date=>(
+                        <li>
+                            {date.daysUntil}
+                        </li>
+                    ))}
                 </ul>
             </div>
         )
